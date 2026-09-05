17d55e38cfeaaabd03e2c677596dcb73699f773a
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-05 08:15:39 +0100

    Change how data is relayed around the widget

--- widgets/tkInputBox.py
+++ widgets/tkInputBox.py
@@ -8,13 +8,16 @@ class tkInputBox():
 		snd={}
 		#print("Do stuff here") 
 		for i,j in self.vars.items():
-			if (hasattr(j, "get")):
-				snd[i] = j.get()
+			if (hasattr(j["obj"], "get")):
+				snd[i] = j["obj"].get()
 			elif (i in self.a and self.a[i]["type"] == "treeview"):
-				snd[i] = j.selection()
+				snd[i] = j["obj"].selection()
 			else:
-				snd[i] = None
-		
+				if ("selected" in j):
+					snd[i] = j["selected"]
+				else:
+					end[i] = None
+					
 		for i in self.funcs["add"]:
 			i(snd)
 		self.win.destroy()
@@ -28,18 +31,18 @@ class tkInputBox():
 		for i,j in a.items():
 			#Only supports Entry at the moment!
 			tkinter.Label(self.win, text=j["name"]).grid(column=0,row=h, sticky="e")
-				
+			self.vars[i] = j
 			if ("type" in j and j["type"] == "treeview"):
-				self.vars[i] = ttk.Treeview(self.win)
+				self.vars[i]["obj"] = ttk.Treeview(self.win)
 				for k,l in j["items"].items():
-					self.vars[i].insert("", tkinter.END, k, text=l)
-				self.vars[i].grid(column=1,row=h,pady=5,padx=5, sticky="ew")
+					self.vars[i]["obj"].insert("", tkinter.END, k, text=l)
+				self.vars[i]["obj"].grid(column=1,row=h,pady=5,padx=5, sticky="ew")
 			elif("type" in j and j["type"] == "label"):
-				self.vars[i] = tkinter.Label(self.win, text = j['text'])
-				self.vars[i].grid(column=1,row=h,pady=5,padx=5, sticky="w")
+				self.vars[i]["obj"] = tkinter.Label(self.win, text = j['text'])
+				self.vars[i]["obj"].grid(column=1,row=h,pady=5,padx=5, sticky="w")
 			else:
-				self.vars[i] = tkinter.Entry(self.win)
-				self.vars[i].grid(column=1,row=h,pady=5,padx=5, sticky="w")
+				self.vars[i]["obj"] = tkinter.Entry(self.win)
+				self.vars[i]["obj"].grid(column=1,row=h,pady=5,padx=5, sticky="w")
 			h+= 1
 		tkinter.Button(self.win, text="Add", command=self.add).grid(column=1,row=h)
 		self.win.columnconfigure(1, weight=1)
