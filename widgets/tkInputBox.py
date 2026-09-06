cb6bd1502bd13acae73dd917de6976aeaca7d8d1
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-06 20:57:29 +0100

    Changed treeView to treeViewWithSearch

--- widgets/tkInputBox.py
+++ widgets/tkInputBox.py
@@ -1,24 +1,25 @@
 import tkinter
 import tkinter.ttk as ttk
+from widgets.treeViewWithSearch import treeViewWithSearch
 class tkInputBox():
 	def passFunc(self, loc, func):
 		self.funcs[loc].append(func)
 		
-	def add(self):
+	def add(self, mode):
 		snd={}
 		#print("Do stuff here") 
 		for i,j in self.vars.items():
 			if (hasattr(j["obj"], "get")):
 				snd[i] = j["obj"].get()
 			elif (i in self.a and self.a[i]["type"] == "treeview"):
-				snd[i] = j["obj"].selection()
+				snd[i] = j["obj"].selected()
 			else:
 				if ("selected" in j):
 					snd[i] = j["selected"]
 				else:
 					snd[i] = None
 					
-		for i in self.funcs["add"]:
+		for i in self.funcs[mode]:
 			i(snd)
 		self.win.destroy()
 		
@@ -33,9 +34,11 @@ class tkInputBox():
 			tkinter.Label(self.win, text=j["name"]).grid(column=0,row=h, sticky="e")
 			self.vars[i] = j
 			if ("type" in j and j["type"] == "treeview"):
-				self.vars[i]["obj"] = ttk.Treeview(self.win)
+				self.vars[i]["obj"] = treeViewWithSearch(self.win,0)
+				#self.vars[i]["obj"] = ttk.Treeview(self.win)
 				for k,l in j["items"].items():
-					self.vars[i]["obj"].insert("", tkinter.END, k, text=l)
+					#self.vars[i]["obj"].insert("", tkinter.END, k, text=l)
+					self.vars[i]["obj"].add([k,l])
 				self.vars[i]["obj"].grid(column=1,row=h,pady=5,padx=5, sticky="ew")
 			elif("type" in j and j["type"] == "label"):
 				self.vars[i]["obj"] = tkinter.Label(self.win, text = j['text'])
@@ -46,5 +49,5 @@ class tkInputBox():
 					self.vars[i]["obj"].insert(0, self.vars[i]["text"])
 				self.vars[i]["obj"].grid(column=1,row=h,pady=5,padx=5, sticky="w")
 			h+= 1
-		tkinter.Button(self.win, text="Add", command=self.add).grid(column=1,row=h)
+		tkinter.Button(self.win, text="Add", command=lambda: self.add("add")).grid(column=1,row=h)
 		self.win.columnconfigure(1, weight=1)
