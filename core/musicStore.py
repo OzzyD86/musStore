62c22f5a94244c1cd0d1007a35c7be6afd680a07
OzzyD86 <129894672+OzzyD86@users.noreply.github.com>
2026-09-07 03:38:42 +0100

    Music store (#13)
    * MusicStore
    
    * Moving MusicStore to core
    
    * Calling loader from musicStore
    
    Refactor loader handling to use win.core.loaders instead of ll.
    
    * Missed the core system
    
    Don't know how I managed that!

---
+++ core/musicStore.py
@@ -0,0 +1,16 @@
+import sqlite3
+from core.bindings import bindings
+
+class musStore():
+	def __init__(self):
+		self.con = sqlite3.connect("musScore.db") # Should change this at some point!
+		self.cur = self.con.cursor()
+		self.bindings = bindings()
+		self.loaders = loader()
+
+class loader():
+	def __init__(self):
+		self.loaders = {}
+	
+	def is_loaded(self, mod):
+		return (mod in list(self.loaders.keys()))
